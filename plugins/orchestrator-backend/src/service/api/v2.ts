200b132d8154c3cd19bc33afce217e6d7708a191
Jude Niroshan <jude.niroshan11@gmail.com>
2024-02-26 22:29:29 +0100

    refactor: introduce mapToWorkflowRunStatusDTO mapper

--- plugins/orchestrator-backend/src/service/api/v2.ts
+++ plugins/orchestrator-backend/src/service/api/v2.ts
@@ -27,6 +27,7 @@ import {
   mapToWorkflowDTO,
   mapToWorkflowListResultDTO,
   mapToWorkflowOverviewDTO,
+  mapToWorkflowRunStatusDTO,
   mapToWorkflowSpecFileDTO,
 } from './mapping/V2Mappings';
 import { V1 } from './v1';
@@ -207,22 +208,13 @@ export namespace V2 {
   }
 
   export async function getWorkflowStatuses(): Promise<WorkflowRunStatusDTO[]> {
-    type Capitalized<S extends string> = Capitalize<Lowercase<S>>;
-    const capitalize = <S extends string>(text: S): Capitalized<S> =>
-      (text[0].toUpperCase() + text.slice(1).toLowerCase()) as Capitalized<S>;
-    const result: WorkflowRunStatusDTO[] = [
+    return [
       ProcessInstanceState.Active,
       ProcessInstanceState.Error,
       ProcessInstanceState.Completed,
       ProcessInstanceState.Aborted,
       ProcessInstanceState.Suspended,
-    ].map(
-      (status): WorkflowRunStatusDTO => ({
-        key: capitalize(status),
-        value: status,
-      }),
-    );
-    return result;
+    ].map(status => mapToWorkflowRunStatusDTO(status));
   }
 
   export function extractQueryParam(
